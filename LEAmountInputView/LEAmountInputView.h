5e97a24f1763df4656f4015af1ec40c65c99f822
Lasha Efremidze <efremidzel@hotmail.com>
2015-06-12 00:57:27 -0700

    Released 0.1.6

--- LEAmountInputView/LEAmountInputView.h
+++ LEAmountInputView/LEAmountInputView.h
@@ -10,10 +10,24 @@
 
 #import "LEAmountInputTextField.h"
 
+/**
+ *  The `LEAmountInputView` class is an amount input view inspired by Square's design.
+ */
 @interface LEAmountInputView : UIView
 
+/**
+ *  The textField of the `LEAmountInputView`.
+ */
 @property (nonatomic, strong) LEAmountInputTextField *textField;
 
+/**
+ *  Initializes a new `LEAmountInputView` with the specified frame rectangle and number style.
+ *
+ *  @param frame       The frame rectangle for the view, measured in points.
+ *  @param numberStyle The number style of the view. Sets the numberStyle of the textField's `numberFormatter`.
+ *
+ *  @return An initialized `LEAmountInputView` or nil if couldn't be created.
+ */
 - (instancetype)initWithFrame:(CGRect)frame numberStyle:(NSNumberFormatterStyle)numberStyle;
 
 @end
